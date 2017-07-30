32a618c2bbd76c49b8ccea551d8d013ee835e62d
Tre Murillo <treelzebub@users.noreply.github.com>
2017-07-29 17:27:22 -0700

    anonymouse user identifier for crashlytics

--- app/src/main/kotlin/net/treelzebub/piholder/PiHolderApp.kt
+++ app/src/main/kotlin/net/treelzebub/piholder/PiHolderApp.kt
@@ -5,14 +5,26 @@ import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
 import net.treelzebub.piholder.env.CurrentEnv
 import net.treelzebub.piholder.env.PiHolderEnv
+import net.treelzebub.piholder.prefs.withPref
+import java.util.*
 
 class PiHolderApp : Application() {
+
+    private var userId by withPref<String>("piholder_anonymous_user_identifier")
 
     override fun onCreate() {
         super.onCreate()
         PiHolderEnv.env = CurrentEnv(BuildConfig::class.java)
         if (PiHolderEnv.env.isProd) {
-            Fabric.with(this, Crashlytics())
+            initFabric()
         }
+    }
+
+    private fun initFabric() {
+        Fabric.with(this, Crashlytics())
+        if (userId == null) {
+            userId = UUID.randomUUID().toString()
+        }
+        Crashlytics.setUserIdentifier(userId)
     }
 }
